83ff6fe927cc6fa10ac5db2207d7d26b66a34afd
purva jantikar <purva.1rn15cs083@gmail.com>
2018-07-19 12:41:34 +0530

    pulled

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import psutil
 import webbrowser
 import difflib
 import math
-<<<<<<< HEAD
-=======
-import vlc
->>>>>>> 82bec569bf498995662adf785a45fe5d5a85320d
 
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
@@ -25,7 +21,6 @@ def get_dialog_state():
 	return session['dialogState']
 
 
-
 @ask.launch
 def launch():
 	speech_text = 'Launching Protocol'
@@ -39,7 +34,6 @@ def sectohour(secs):
 	return (hh1, mm, ss)
 
 
-
 @ask.intent('retrieve_percentage')
 def ret_battery():
 	battery = psutil.sensors_battery()
@@ -51,47 +45,17 @@ def ret_battery():
 	return question(speech_text)
 
 	
-		
-def sectohour(secs):
-    mm, ss = divmod(secs, 60) # function to convert from secs to hours
-    hh, mm = divmod(mm, 60)
-    hh1 = int(math.fabs(hh))
-    return (hh1, mm, ss)
-   
-  
-@ask.intent('retrieve_timeleft')
-def ret_timeleft():
-	battery = psutil.sensors_battery()
-	(hh, mm, ss) = sectohour(battery.secsleft)
-	speech_text = 'time left is %s hour %s minutes'%(hh, mm)
-	return question(speech_text)
-	
-	
-@ask.intent('charge_status') # to check if laptop is put on charge or not
-def ret_plugstatus():
-	battery = psutil.sensors_battery()
-	if(battery.power_plugged == True):
-		speech_text = 'yes your laptop is on charge'
-	else:
-		speech_text = 'no your laptop is not on charge'
-	return question(speech_text)
-	
-	
 @ask.intent('access_file')
 def accessfile(file_name):
-<<<<<<< HEAD
 	dialog_state = get_dialog_state()
 	if dialog_state != 'COMPLETED':
 		return delegate()
-=======
->>>>>>> 82bec569bf498995662adf785a45fe5d5a85320d
 	os.system('gedit "{0}"'.format(file_name))
 	speech_text = 'file open %s'%file_name
 	return question(speech_text)
 
 
 @ask.intent('play_file')
-<<<<<<< HEAD
 def play_music(file_type,file_name):
 	dialog_state = get_dialog_state()
 	if dialog_state != 'COMPLETED':
@@ -130,38 +94,11 @@ def play_music(file_type,file_name):
 	#os.system(start 'C:\pathtotool.exe -2 c:\data')
 	#speech_text = 'file running %s'%file_name
 	#return question(speech_text)
-=======
-def play_music(file_name):
-	to_search = file_name + '.mp3'
-	path = '/home/purva/videos'
-	for filename in os.listdir(path):
-		d = difflib.SequenceMatcher(None, filename, to_search).ratio()
-		if d>=0.6:
-			matches=(os.path.join(path, filename))
-	webbrowser.open(matches)
->>>>>>> 82bec569bf498995662adf785a45fe5d5a85320d
 
 @ask.intent('phone_buzz')
 def call_my_phone():
 	speech_text = "Calling your Phone now"
 	client.calls.create(to="+918147486031", from_=TWILIO_PHONE_NUMBER,url=WIML_INSTRUCTIONS_URL, method="GET")
-	return question(speech_text)
-
-@ask.intent('play_video') #intent name
-def play_vdieo(file_name):
-	to_search = file_name + '.mp4'
-	path = '/home/purva/videos'
-	for filename in os.listdir(path):
-		d = difflib.SequenceMatcher(None, filename, to_search).ratio()
-		if d>=0.6:
-			matches=(os.path.join(path, filename))
-	webbrowser.open(matches)
-	
-
-@ask.intent('') # intent name
-def execute_file(file_name):
-	os.system(start 'C:\pathtotool.exe -2 c:\data')
-	speech_text = 'file running %s'%file_name
 	return question(speech_text)
 
 
@@ -175,4 +112,4 @@ if __name__ == '__main__':
 		verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
 		if verify == 'false':
 			app.config['ASK_VERIFY_REQUESTS'] = False
-	app.run(debug=True, port=5001)
+app.run(debug=True, port=5001)
